a40f77e045ef514a2758c7263b99a2c44fc99f47
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2025-04-16 13:46:02 +0300

    added login page

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ MaterialModule, ReactiveFormsModule, NgIf ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {}
+
+  login() {
+    console.log(this.loginForm.value);
+    this.authService.login(this.loginForm.value).subscribe((data) => {
+      console.log(data);
+      this.authService.setToken(data.token);
+      this.router.navigate(['/']);
+    }, (error: HttpErrorResponse) => {
+      console.log(error.error.errors);
+    });
+  }
+}
---
+++ src/app/auth/login/token.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        const token = this.authService.getToken();
+
+        if(token) {
+            const cloned = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${token}`)
+            });
+            return next.handle(cloned);
+        }
+
+        return next.handle(req);
+    }
+}
